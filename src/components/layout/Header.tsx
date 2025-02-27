a4aa96fa94833dfbb434addaa6c6efc101bddbf3
Rodrigo Granada <rodrigotgranada@gmail.com>
2025-02-27 16:07:39 -0300

    Ajuste no calculo do Header e Main para responsividade

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,10 +4,10 @@ import Logo from "../logo/Logo";
 
 const Header: React.FC = () => {
   return (
-    <header className="w-full bg-white shadow-md flex items-center px-[0.651rem] md:px-[1.375rem] top-0 left-0 z-50">
-      <div className="container mx-auto flex justify-between items-center py-4">
+    <header className="w-full bg-white shadow-md flex items-center px-[calc(1rem-10px)] sm:px-[calc(2rem-10px)] md:px-[calc(4.375rem-10px)] lg:px-[calc(6rem-10px)] xl:px-[calc(8rem-10px)] top-0 left-0 z-50">
+      <div className="w-full flex justify-between items-center py-4">
         <Link to="/" className="flex items-center">
-          <Logo className="w-24 h-auto" />
+          <Logo className="w-24 h-auto ml-[10px]" /> {/* Ajuste do logo */}
         </Link>
       </div>
     </header>
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,7 +5,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col bg-gray00">
       <Header />
-      <main className="flex-grow container mx-auto p-0 px-[1.25rem] md:px-[2rem] w-full bg-gray00">
+      <main className="flex-grow w-full bg-gray00 px-[calc(1rem+10px)] sm:px-[calc(2rem+10px)] md:px-[calc(4.375rem+10px)] lg:px-[calc(6rem+10px)] xl:px-[calc(8rem+10px)] container mx-auto">
         {children}
       </main>
     </div>
